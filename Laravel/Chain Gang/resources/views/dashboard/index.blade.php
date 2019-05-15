c8a542d1335b6369bc83bbca4c56b798d926ec90
MrMoosti <meyilmaz2001@hotmail.com>
2019-05-15 12:49:56 +0200

    fix

---
+++ Laravel/Chain Gang/resources/views/dashboard/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+    @include('dashboard.header.header')
+    <body class="app">
+        <div id="loader">
+            <div class="spinner"></div>
+        </div>
+        <script>
+            window.addEventListener('load', function load() {
+            const loader = document.getElementById('loader');
+            setTimeout(function() {
+                loader.classList.add('fadeOut');
+            }, 300);
+            });
+        </script>
+        @include('dashboard.sidepanel.view')
+        @yield('body')
+    </body>
+</html>
+
+{{-- @section('body')
+    
+@show --}}
+
+{{-- @yield('body') To import from sections--}}
