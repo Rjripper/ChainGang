a82e4c1473b9991e27de92b0a43c31722a6e3158
MrMoosti <meyilmaz2001@hotmail.com>
2019-05-15 12:24:14 +0200

    Dashboard admin import project fix

--- Laravel/Chain Gang/resources/views/dashboard/index.blade.php
+++ Laravel/Chain Gang/resources/views/dashboard/index.blade.php
@@ -1,5 +1,23 @@
-@include('dashboard.header.view')
-@include('dashboard.sidepanel.view')
+
+<!DOCTYPE HTML>
+<html>
+    @include('dashboard.header.header')
+    <body class="app">
+        <div id="loader">
+            <div class="spinner"></div>
+        </div>
+        <script>
+            window.addEventListener('load', function load() {
+            const loader = document.getElementById('loader');
+            setTimeout(function() {
+                loader.classList.add('fadeOut');
+            }, 300);
+            });
+        </script>
+        @include('dashboard.sidepanel.view')
+        @yield('body')
+    </body>
+</html>
 
 {{-- @section('body')
     
