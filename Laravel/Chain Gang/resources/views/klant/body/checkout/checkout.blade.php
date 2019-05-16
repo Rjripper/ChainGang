6eeafc2e3d08cae11ae13d2a1a273170006600b2
KillStreamNL <joost.schooltink@student.graafschapcollege.nl>
2019-05-16 09:57:00 +0200

    Checkout blade gemaakt

---
+++ Laravel/Chain Gang/resources/views/klant/body/checkout/checkout.blade.php
@@ -0,0 +1,212 @@
+	<!-- BREADCRUMB -->
+	<div id="breadcrumb">
+		<div class="container">
+			<ul class="breadcrumb">
+				<li><a href="#">Home</a></li>
+				<li class="active">Checkout</li>
+			</ul>
+		</div>
+	</div>
+	<!-- /BREADCRUMB -->
+
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<form id="checkout-form" class="clearfix">
+					<div class="col-md-6">
+						<div class="Adres gegevens">
+							<p>Bent u al klant ? <a href="#">Login</a></p>
+							<div class="section-title">
+								<h3 class="title">Adres gegevens</h3>
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="first-name" placeholder="Voornaam">
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="last-name" placeholder="Achternaam">
+							</div>
+							<div class="form-group">
+								<input class="input" type="email" name="email" placeholder="Email">
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="address" placeholder="Adres">
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="city" placeholder="Woonplaats">
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="country" placeholder="Land">
+							</div>
+							<div class="form-group">
+								<input class="input" type="text" name="zip-code" placeholder="Postcode">
+							</div>
+							<div class="form-group">
+								<input class="input" type="tel" name="tel" placeholder="Telefoon">
+							</div>
+							<div class="form-group">
+								<div class="input-checkbox">
+									<input type="checkbox" id="register">
+									<label class="font-weak" for="register">Account aanmaken?</label>
+									<div class="caption">
+                                        <p>Lorem Bla die bla opvul tekst iets kijken waar dit staat enzo later!.</p>
+                                        <input class="input" type="password" name="password" placeholder="Vul uw wachtwoord in">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="shiping-methods">
+							<div class="section-title">
+								<h4 class="title">Verzend mogelijkeden</h4>
+							</div>
+							<div class="input-checkbox">
+								<input type="radio" name="shipping" id="shipping-1" checked>
+								<label for="shipping-1">Gratis verzending - €0.00</label>
+								<div class="caption">
+									<p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+								</div>
+							</div>
+							<div class="input-checkbox">
+								<input type="radio" name="shipping" id="shipping-2">
+								<label for="shipping-2">Standaard - €4.00</label>
+								<div class="caption">
+									<p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+								</div>
+							</div>
+						</div>
+
+						<div class="payments-methods">
+							<div class="section-title">
+								<h4 class="title">Betaal mogelijkheden</h4>
+							</div>
+							<div class="input-checkbox">
+								<input type="radio" name="payments" id="payments-1" checked>
+								<label for="payments-1">Directe bank overboeking</label>
+								<div class="caption">
+									<p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+								</div>
+							</div>
+							<div class="input-checkbox">
+								<input type="radio" name="payments" id="payments-2">
+								<label for="payments-2">Ideal</label>
+								<div class="caption">
+									<p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+								</div>
+							</div>
+							<div class="input-checkbox">
+								<input type="radio" name="payments" id="payments-3">
+								<label for="payments-3">Paypal</label>
+								<div class="caption">
+									<p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                    </p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="order-summary clearfix">
+							<div class="section-title">
+								<h3 class="title">Controleer order</h3>
+							</div>
+							<table class="shopping-cart-table table">
+								<thead>
+									<tr>
+										<th>Product</th>
+										<th></th>
+										<th class="text-center">Prijs</th>
+										<th class="text-center">Aantal</th>
+										<th class="text-center">Totaal</th>
+										<th class="text-right"></th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td class="thumb"><img src="./img/thumb-product01.jpg" alt=""></td>
+										<td class="details">
+											<a href="#">Product naam komt hier</a>
+											<ul>
+												<li><span>Maat: XL</span></li>
+												<li><span>Kleur: Camelot</span></li>
+											</ul>
+										</td>
+										<td class="price text-center"><strong>€32.50</strong><br><del class="font-weak"><small>€40.00</small></del></td>
+										<td class="qty text-center"><input class="input" type="number" value="1"></td>
+										<td class="total text-center"><strong class="primary-color">€32.50</strong></td>
+										<td class="text-right"><button class="main-btn icon-btn"><i class="fa fa-close"></i></button></td>
+									</tr>
+									<tr>
+                                            <td class="thumb"><img src="./img/thumb-product01.jpg" alt=""></td>
+                                            <td class="details">
+                                                <a href="#">Product naam komt hier</a>
+                                                <ul>
+                                                    <li><span>Maat: XL</span></li>
+                                                    <li><span>Kleur: Camelot</span></li>
+                                                </ul>
+                                            </td>
+                                            <td class="price text-center"><strong>€32.50</strong><br><del class="font-weak"><small>€40.00</small></del></td>
+                                            <td class="qty text-center"><input class="input" type="number" value="1"></td>
+                                            <td class="total text-center"><strong class="primary-color">€32.50</strong></td>
+                                            <td class="text-right"><button class="main-btn icon-btn"><i class="fa fa-close"></i></button></td>
+                                        </tr>
+                                        <tr>
+                                                <td class="thumb"><img src="./img/thumb-product01.jpg" alt=""></td>
+                                                <td class="details">
+                                                    <a href="#">Product naam komt hier</a>
+                                                    <ul>
+                                                        <li><span>Maat: XL</span></li>
+                                                        <li><span>Kleur: Camelot</span></li>
+                                                    </ul>
+                                                </td>
+                                                <td class="price text-center"><strong>€32.50</strong><br><del class="font-weak"><small>€40.00</small></del></td>
+                                                <td class="qty text-center"><input class="input" type="number" value="1"></td>
+                                                <td class="total text-center"><strong class="primary-color">€32.50</strong></td>
+                                                <td class="text-right"><button class="main-btn icon-btn"><i class="fa fa-close"></i></button></td>
+                                            </tr>
+								</tbody>
+								<tfoot>
+									<tr>
+										<th class="empty" colspan="3"></th>
+										<th>SUBTOTAAL</th>
+										<th colspan="2" class="sub-total">$97.50</th>
+									</tr>
+									<tr>
+										<th class="empty" colspan="3"></th>
+										<th>VERZENDKOSTEN</th>
+										<td colspan="2">Free Shipping</td>
+									</tr>
+									<tr>
+										<th class="empty" colspan="3"></th>
+										<th>TOTAAL</th>
+										<th colspan="2" class="total">$97.50</th>
+									</tr>
+								</tfoot>
+							</table>
+							<div class="pull-right">
+								<button class="primary-btn">Plaats bestelling</button>
+							</div>
+						</div>
+
+					</div>
+				</form>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /section -->
