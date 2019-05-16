bb940ad4824aa90db6e62a22ae93ff6d931035b7
Rj sw <robertjan.schmidtwe@student.graafschapcollege.nl>
2019-05-16 08:50:56 +0200

    First push index

--- Laravel/Chain Gang/resources/views/dashboard/index.blade.php
+++ Laravel/Chain Gang/resources/views/dashboard/index.blade.php
@@ -1,7 +1,26 @@
-@include('header')
-
-@include('sidepanel')
-
-    @yield('body')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <style>#loader{transition:all .3s ease-in-out;opacity:1;visibility:visible;position:fixed;height:100vh;width:100%;background:#fff;z-index:90000}#loader.fadeOut{opacity:0;visibility:hidden}.spinner{width:40px;height:40px;position:absolute;top:calc(50% - 20px);left:calc(50% - 20px);background-color:#333;border-radius:100%;-webkit-animation:sk-scaleout 1s infinite ease-in-out;animation:sk-scaleout 1s infinite ease-in-out}@-webkit-keyframes sk-scaleout{0%{-webkit-transform:scale(0)}100%{-webkit-transform:scale(1);opacity:0}}@keyframes sk-scaleout{0%{-webkit-transform:scale(0);transform:scale(0)}100%{-webkit-transform:scale(1);transform:scale(1);opacity:0}}</style>
     
-@include('footer')
+</head>
+<body>
+        @include('dashboard.header.header')
+
+        @include('dashboard.sidepanel.sidepanel')
+
+        @section('body')
+        @show 
+
+    
+</body>
+</html>
+
+
+
+
+    
--- Laravel/Chain Gang/routes/web.php
+++ Laravel/Chain Gang/routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard.index');
 });
+
+
