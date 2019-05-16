43c15648014d85f039c19a1d635662cd1d7fad38
KillStreamNL <joost.schooltink@student.graafschapcollege.nl>
2019-05-16 09:09:12 +0200

    Footer added JS

--- Laravel/Chain Gang/resources/views/klant/footer/copyright.blade.php
+++ Laravel/Chain Gang/resources/views/klant/footer/copyright.blade.php
@@ -17,12 +17,12 @@
 	<!-- /FOOTER -->
 
 	<!-- jQuery Plugins -->
-	<script src="js/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/slick.min.js"></script>
-	<script src="js/nouislider.min.js"></script>
-	<script src="js/jquery.zoom.min.js"></script>
-	<script src="js/main.js"></script>
+	<script src="{{ asset('js/customer/initial/js/jquery.min.js') }}"></script>
+	<script src="{{ asset('js/customer/initial/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('js/customer/initial/js/slick.min.js') }}"></script>
+	<script src="{{ asset('js/customer/initial/js/nouislider.min.js') }}"></script>
+	<script src="{{ asset('js/customer/initial/js/jquery.zoom.min.js') }}"></script>
+	<script src="{{ asset('js/customer/initial/js/main.js') }}"></script>
 
 </body>
 
