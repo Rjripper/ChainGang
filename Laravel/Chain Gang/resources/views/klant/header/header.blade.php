1c1894dd1e28afd7836c9f316e270ae9df02cb58
TKremer00 <tim.kremer1@student.graafschapcollege.nl>
2019-05-19 20:35:32 +0200

    changed account menu

--- Laravel/Chain Gang/resources/views/klant/header/header.blade.php
+++ Laravel/Chain Gang/resources/views/klant/header/header.blade.php
@@ -35,8 +35,6 @@
 								<a href="#" class="text-uppercase">Login</a> / <a href="#" class="text-uppercase">Join</a>
 								<ul class="custom-menu">
 									<li><a href="#"><i class="fa fa-user-o"></i> My Account</a></li>
-									<li><a href="#"><i class="fa fa-heart-o"></i> My Wishlist</a></li>
-									<li><a href="#"><i class="fa fa-exchange"></i> Compare</a></li>
 									<li><a href="#"><i class="fa fa-check"></i> Checkout</a></li>
 									<li><a href="#"><i class="fa fa-unlock-alt"></i> Login</a></li>
 									<li><a href="#"><i class="fa fa-user-plus"></i> Create An Account</a></li>
