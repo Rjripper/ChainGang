f4327ad6edb5365bce85cdf25d12cb18c2d00287
MrMoosti <meyilmaz2001@hotmail.com>
2019-05-19 11:05:16 +0200

    fix: selected sidebar link indicator

--- Laravel/Chain Gang/resources/views/dashboard/sidepanel/view.blade.php
+++ Laravel/Chain Gang/resources/views/dashboard/sidepanel/view.blade.php
@@ -20,7 +20,7 @@
             </div>
         </div>
         <ul class="sidebar-menu scrollable pos-r">
-            <li class="nav-item mT-30 active"><a class="sidebar-link" href="{{ route ('dashboard') }}"><span class="icon-holder"><i class="c-blue-500 ti-home"></i> </span><span class="title">Dashboard</span></a></li>
+            <li class="nav-item mT-30 actived"><a class="sidebar-link actived" href="{{ route ('dashboard') }}"><span class="icon-holder"><i class="c-blue-500 ti-home"></i> </span><span class="title">Dashboard</span></a></li>
             <li class="nav-item"><a class="sidebar-link" href="{{ route ('users') }}"><span class="icon-holder"><i class="c-brown-500 ti-user"></i> </span><span class="title">Gebruikers</span></a></li>
             <li class="nav-item"><a class="sidebar-link" href="{{ route ('customers') }}"><span class="icon-holder"><i class="c-blue-500 ti-info-alt"></i> </span><span class="title">Klanten</span></a></li>
             <li class="nav-item"><a class="sidebar-link" href="{{ route ('products') }}"><span class="icon-holder"><i class="c-deep-orange-500 ti-archive"></i> </span><span class="title">Producten</span></a></li>
