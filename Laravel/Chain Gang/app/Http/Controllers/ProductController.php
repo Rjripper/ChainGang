9052ed9b7dda37222af6078894099f3f7762ca81
MrMoosti <meyilmaz2001@hotmail.com>
2019-05-22 18:53:17 +0200

    quick product controller thing

--- Laravel/Chain Gang/app/Http/Controllers/ProductController.php
+++ Laravel/Chain Gang/app/Http/Controllers/ProductController.php
@@ -7,17 +7,25 @@ use App\Product;
 
 class ProductController extends Controller
 {
-    public function  index()
+    public function index()
     {
         // Get 9 Products
         // Paginate It
         // Return view with products
-        
+
         // Loop them in the view
         // add the urls to add them to cart
 
         $products = Product::paginate(9);
 
-        return view('klant.body.products.products', compact('products'));
+        return view('', compact('products'));
+    }
+
+    public function show(Request $request, Product $product)
+    {
+        //Get Product to show from route {product}
+        //Return the view with the product id
+
+        return view('klant.body.product-details.details', compact('product'));
     }
 }
