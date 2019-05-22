50fc5e3a78b7b3c4be1b86cbb5a8526b31a76f60
KillStreamNL <joost.schooltink@student.graafschapcollege.nl>
2019-05-22 11:02:45 +0200

    tables created

---
+++ Laravel/Chain Gang/database/migrations/2019_05_22_083445_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+            $table->string('sale');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
---
+++ Laravel/Chain Gang/database/migrations/2019_05_22_085423_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+            $table->integer('rating',5);
+            $table->string('title');
+            $table->string('message');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
