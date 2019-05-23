0b2c56802ad0c3e129f6708ddc360ea78c53fbf3
TKremer00 <tim.kremer1@student.graafschapcollege.nl>
2019-05-23 18:50:26 +0200

    product images af

--- Laravel/Chain Gang/resources/views/klant/body/product-details/product.blade.php
+++ Laravel/Chain Gang/resources/views/klant/body/product-details/product.blade.php
@@ -1,10 +1,11 @@
  {{-- big images --}}
 @php
     $images_for_product = App\ProductImage::Where('product_id', $product->id)->get('image');
+    
 @endphp
 
  <div id="product-main-view">
-    @for ($i = 0; $i < 3; $i++) 
+    @for ($i = 0; $i < count($images_for_product); $i++) 
         <div class="product-view">
             <img src="{{ asset($images_for_product[$i]->image ) }}" alt="">
         </div>
@@ -13,7 +14,7 @@
     
 {{-- more images --}}
 <div id="product-view">
-    @for ($i = 0; $i < 3; $i++) 
+    @for ($i = 0; $i < count($images_for_product); $i++) 
         <div class="product-view">
             <img src="{{ asset($images_for_product[$i]->image ) }}" alt="">
         </div>
